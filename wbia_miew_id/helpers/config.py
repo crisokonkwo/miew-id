ff1cff2c52f4c35813c8a0ab229fa1308f3a3e59
AnkitKUpadhyay <61064247+AnkitKUpadhyay@users.noreply.github.com>
2024-03-14 21:41:19 -0400

    Update config.py

--- wbia_miew_id/helpers/config.py
+++ wbia_miew_id/helpers/config.py
@@ -32,23 +32,18 @@ class Test(DictableClass):
     eval_groups: List = field(default_factory=list)
 
 @dataclass
-class PreprocessImages(DictableClass):
-    apply: bool = False
-    preprocessed_dir: str = None
-    force_apply: bool = False
-
-@dataclass
 class Data(DictableClass):
     images_dir: str
     train: Train
     val: Val
-    preprocess_images: PreprocessImages
     image_size: Tuple[int, int]
     test: Test = None
     viewpoint_list: List = None
     name_keys: List = field(default_factory=lambda: ['name'])
     crop_bbox: bool = False
     use_full_image_path: bool = False
+    preprocess_images: bool = False
+
 
 @dataclass
 class Engine(DictableClass):
@@ -60,12 +55,6 @@ class Engine(DictableClass):
     use_wandb: bool
     num_workers: int = 0
     loss_module: str = 'softmax'
-    use_swa: bool = False
-
-@dataclass
-class SWAParams(DictableClass):
-    swa_lr: float = 0.00014
-    swa_start: int = 21
 
 @dataclass
 class SchedulerParams(DictableClass):
@@ -85,12 +74,11 @@ class ModelParams(DictableClass):
     loss_module: str
     s: float
     margin: float
-    ls_eps: float
-    theta_zero: float
     pretrained: bool
     n_classes: int
-    k: int = 2
-    
+    #k: int = 2
+    theta_zero: float
+
 @dataclass
 class TestParams():
     batch_size: int = 4
@@ -108,8 +96,7 @@ class Config(DictableClass):
     scheduler_params: SchedulerParams
     model_params: ModelParams
     test: TestParams
-    swa_params: SWAParams
-
+    
 
 
 def load_yaml(file_path: str) -> Dict:
@@ -152,19 +139,11 @@ def get_config(file_path: str) -> Config:
         print("Attempting to convert config dict to compatible format...")
         config_dict = convert_config_dict(config_dict)
 
-    if not config_dict.get('swa_params', False):
-        config_dict['swa_params'] = dict(SWAParams())
-
-    if not config_dict['data'].get('preprocess_images', False) or isinstance(config_dict['data']['preprocess_images'], bool):
-        config_dict['data']['preprocess_images'] = dict(PreprocessImages())
-
     config_dict['data'] = Data(**config_dict['data'])
     config_dict['data'].train = Train(**config_dict['data'].train)
     config_dict['data'].val = Val(**config_dict['data'].val)
     config_dict['data'].test = Test(**config_dict['data'].test)
-    config_dict['data'].preprocess_images = PreprocessImages(**config_dict['data'].preprocess_images)
     config_dict['engine'] = Engine(**config_dict['engine'])
-    config_dict['swa_params'] = SWAParams(**config_dict['swa_params'])
     config_dict['scheduler_params'] = SchedulerParams(**config_dict['scheduler_params'])
     config_dict['model_params'] = ModelParams(**config_dict['model_params'])
     config_dict['test'] = TestParams(**config_dict['test'])
